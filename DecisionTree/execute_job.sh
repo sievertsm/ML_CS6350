2a18858e9b171ea41af45dfac1ca9f1fad731a31
sievertsm <50172661+sievertsm@users.noreply.github.com>
2021-02-22 14:46:34 -0700

    Add files via upload
    added sh file to run the python scripts for problem 2.2, 2.3a, and 2.3b.

---
+++ DecisionTree/execute_job.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+printf "\nRunning Problem 2.2\n"
+python problem2_2.py
+
+printf "\nRunning Problem 2.3a\n"
+python problem2_3a.py
+
+printf "\nRunning Problem 2.3b\n"
+python problem2_3b.py
