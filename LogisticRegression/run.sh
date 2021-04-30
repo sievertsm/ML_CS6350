c8a36665df1e37dae1626041c14b3ca1d50f25ab
sievertsm <sievertsm@gmail.com>
2021-04-30 09:03:51 -0600

    added run.sh file

---
+++ LogisticRegression/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+printf "\nRunning Problem 2 Logistic Regression\n"
+python problem_2.py
---
+++ NeuralNetwork/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+printf "\nRunning Problem 3 Artificial Neural Network\n"
+python problem_3.py
